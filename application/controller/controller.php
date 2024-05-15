7d42dde84aad73f987bab458ed5705f7ea00df62
my276 <116759034+JiuMing2026@users.noreply.github.com>
2024-05-15 04:34:24 +0100

    Update controller.php

--- application/controller/controller.php
+++ application/controller/controller.php
@@ -10,27 +10,22 @@ class Controller {
 
         $this->$pageURI();
     }
-
     function home() {
-        $this->load->view('view3DApp');
+        $this->load->view('view3DAppData');
     }
 
-//    function apiCreateTable() {
-//        $data = $this->model->dbCreateTable();
-//        $this->load->view('viewMessage', $data);
-//    }
-//
-//    function apiInsertData() {
-//        $data = $this->model->dbInsertData();
-//        $this->load->view('view3DAppData', $data);
-//    }
-//
-//    function apiGetData() {
-//        $data = $this->model->dbGetData();
-//        $this->load->view('view3DApp', $data);
-//    }
-//
-//    function apiGetFlickService() {
-//        $this->load->view('viewFlickService');
-//    }
+    function apiCreateTable() {
+        $data = $this->model->dbCreateTable();
+        $this->load->view('view3DAppData', $data);
+    }
+
+    function apiInsertData() {
+        $data = $this->model->dbInsertData();
+        $this->load->view('view3DAppData', $data);
+    }
+
+    function apiGetData() {
+        $data = $this->model->dbGetData();
+        $this->load->view('view3DAppData', $data);
+    }
 }
